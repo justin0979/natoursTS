e30cfd226cbc7b78c0bdd7e1189a4dee0379ae47
justin0979 <justinm0979@gmail.com>
2021-07-12 20:30:15 -0500

    add img/ and setup Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 export const Header: React.FC = () => {
-  return <div>
-  <h1>Header</h1>
-</div>
-}
-
-
+  return (
+    <header className="header">
+      <h1>Header</h1>
+      some text...
+    </header>
+  );
+};
