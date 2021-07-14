c719a248fd2d92c8a549be8455254779957c40c5
justin0979 <justinm0979@gmail.com>
2021-07-14 12:45:16 -0500

    add and position icon and text in Header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,11 @@
-import { Header } from "&components/Header"
+import React from "react";
+import { Header } from "&components/Header";
 
 const App: React.FC = () => {
   return (
-    <div>
-    <Header />
-    </div>
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
   );
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,17 @@ export const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="logo-box">
-        <img src={logoWhite} alt="" />
+        <img src={logoWhite} alt="Logo" className="logo" />
+      </div>
+      <div className="text-box">
+        <h1 className="heading-primary">
+          <span className="heading-primary-main">
+            Outdoors
+          </span>
+          <span className="heading-primary-sub">
+            is where life happens
+          </span>
+        </h1>
       </div>
     </header>
   );
