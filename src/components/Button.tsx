055072d1d8f684cdda05a8661a43f6483d80792b
justin0979 <justinm0979@gmail.com>
2021-07-16 15:44:31 -0500

    add keyframe animations to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const Button = () => {
   return (
-    <Link className="btn btn-white" to="#">
+    <Link className="btn btn-white btn-animated" to="#">
       Discover our tours
     </Link>
   );
