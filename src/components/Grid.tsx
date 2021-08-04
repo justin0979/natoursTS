1fd45c95b43a25cf8656f5b5e105c1327987f845
justin0979 <justinm0979@gmail.com>
2021-08-04 15:40:03 -0500

    add grid laylout for col-1-of-4 and col-1-of-3, with  variables

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -2,32 +2,32 @@ export const Grid: React.FC = () => {
   return (
     <section id="grid">
       <div className="row">
-        <div className="col-1-of-2 col">col-1-of-2</div>
-        <div className="col-1-of-2 col">col-1-of-2</div>
+        <div className="col-1-of-2">col-1-of-2</div>
+        <div className="col-1-of-2">col-1-of-2</div>
       </div>
       <div className="row">
-        <div className="col-1-of-3 col">col-1-of-3</div>
-        <div className="col-1-of-3 col">col-1-of-3</div>
-        <div className="col-1-of-3 col">col-1-of-3</div>
+        <div className="col-1-of-3">col-1-of-3</div>
+        <div className="col-1-of-3">col-1-of-3</div>
+        <div className="col-1-of-3">col-1-of-3</div>
       </div>
       <div className="row">
-        <div className="col-1-of-3 col">col-1-of-3</div>
-        <div className="col-2-of-3 col">col-2-of-3</div>
+        <div className="col-1-of-3">col-1-of-3</div>
+        <div className="col-2-of-3">col-2-of-3</div>
       </div>
       <div className="row">
-        <div className="col-1-of-4 col">col-1-of-4</div>
-        <div className="col-1-of-4 col">col-1-of-4</div>
-        <div className="col-1-of-4 col">col-1-of-4</div>
-        <div className="col-1-of-4 col">col-1-of-4</div>
+        <div className="col-1-of-4">col-1-of-4</div>
+        <div className="col-1-of-4">col-1-of-4</div>
+        <div className="col-1-of-4">col-1-of-4</div>
+        <div className="col-1-of-4">col-1-of-4</div>
       </div>
       <div className="row">
-        <div className="col-1-of-4 col">col-1-of-4</div>
-        <div className="col-1-of-4 col">col-1-of-4</div>
-        <div className="col-2-of-4 col">col-2-of-4</div>
+        <div className="col-1-of-4">col-1-of-4</div>
+        <div className="col-1-of-4">col-1-of-4</div>
+        <div className="col-2-of-4">col-2-of-4</div>
       </div>
       <div className="row">
-        <div className="col-1-of-4 col">col-1-of-4</div>
-        <div className="col-3-of-4 col">col-3-of-4</div>
+        <div className="col-1-of-4">col-1-of-4</div>
+        <div className="col-3-of-4">col-3-of-4</div>
       </div>
     </section>
   );
