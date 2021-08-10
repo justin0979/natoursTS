c8be10a420db6789586dfcb92a3eafe2df1eba7e
justin0979 <justinm0979@gmail.com>
2021-08-10 18:31:46 -0500

    specify where empty spaces are allowed

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,10 +1,16 @@
 module.exports = {
   extends: "stylelint-config-standard",
   rules: {
+    "at-rule-empty-line-before": null,
     "at-rule-no-unknown": null,
     "color-hex-length": null,
     "declaration-colon-newline-after": null,
-    "declaration-empty-line-before": null,
+    "declaration-empty-line-before": [
+      "never",
+      {
+        ignore: ["after-declaration"],
+      },
+    ],
     indentation: [
       2,
       {
