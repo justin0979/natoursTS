9f83760812627395224923b93f02199991c4427f
justin0979 <justinm0979@gmail.com>
2021-09-04 10:59:54 -0500

    add scaffolding for Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,61 @@
+import logoGreen2x from '&img/logo-green-2x.png';
+
 const Footer: React.FC = () => {
   return (
     <footer id="footer" className="footer">
-      <h1>Footer</h1>
+      <div className="footer__logo-box">
+        <img
+          src={logoGreen2x}
+          alt="Full logo"
+          className="footer__logo"
+        />
+      </div>
+      <div className="row">
+        <div className="col-1-of-2">
+          <div className="footer__navigation">
+            <ul className="footer__list">
+              <li className="footer__item">
+                <a className="footer__link" href="#">
+                  Company
+                </a>
+              </li>
+              <li className="footer__item">
+                <a className="footer__link" href="#">
+                  Contact Us
+                </a>
+              </li>
+              <li className="footer__item">
+                <a className="footer__link" href="#">
+                  Careers
+                </a>
+              </li>
+              <li className="footer__item">
+                <a className="footer__link" href="#">
+                  Privacy policy
+                </a>
+              </li>
+              <li className="footer__item">
+                <a className="footer__link" href="#">
+                  Terms
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-1-of-2">
+          <p className="footer__copyright">
+            Built my{' '}
+            <a className="footer__link" href="#">
+              Justin
+            </a>{' '}
+            for learning from an online course{' '}
+            <a className="footer__link" href="#">
+              Advanced CSS and Sass
+            </a>
+            . &copy; by Jonas Schmedtmann
+          </p>
+        </div>
+      </div>
     </footer>
   );
 };
