6eb70c4787bea143dc8571f408f9626af8498f03
justin0979 <justinm0979@gmail.com>
2021-09-06 21:42:06 -0500

    add styling to navigation list items

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,15 @@
 const Navigation: React.FC = () => {
   return (
     <div className="navigation">
-      <input className="navigation__checkbox" type="checkbox" />
-      <label className="navigation__button" htmlFor="">
+      <input
+        id="navi-toggle"
+        className="navigation__checkbox"
+        type="checkbox"
+      />
+      <label
+        className="navigation__button"
+        htmlFor="navi-toggle"
+      >
         MENU
       </label>
       <div className="navigation__background">&nbsp;</div>
@@ -10,26 +17,31 @@ const Navigation: React.FC = () => {
         <ul className="navigation__list">
           <li className="navigation__item">
             <a className="navigation__link" href="#">
+              <span>01</span>
               About Natours
             </a>
           </li>
           <li className="navigation__item">
             <a className="navigation__link" href="#">
+              <span>02</span>
               Your benefits
             </a>
           </li>
           <li className="navigation__item">
             <a className="navigation__link" href="#">
+              <span>03</span>
               Popular tours
             </a>
           </li>
           <li className="navigation__item">
             <a className="navigation__link" href="#">
+              <span>04</span>
               Stories
             </a>
           </li>
           <li className="navigation__item">
             <a className="navigation__link" href="#">
+              <span>05</span>
               Book Now
             </a>
           </li>
