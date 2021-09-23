0d88bf0c1061958b7d70c80d7d39f598d862cf4f
justin0979 <justinm0979@gmail.com>
2021-09-23 09:48:41 -0500

    add all styling for popup content

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -20,17 +20,28 @@ const Popup: React.FC = () => {
           />
         </div>
         <div className="popup__right">
-          <Heading type="secondary">Start booking now</Heading>
-          <Heading type="tertiary">
+          <Heading
+            type="secondary"
+            className=""
+            classNameDiv="u-margin-bottom-small"
+          >
+            Start booking now
+          </Heading>
+          <Heading type="tertiary" className="">
             Important &ndash; Please read these terms before
             booking
           </Heading>
           <p className="popup__text">
             Dolor ea in possimus sit libero. Ducimus accusantium
-            quis inventore autem repudiandae Odio doloribus
-            molestiae accusantium voluptatum cumque Eius
-            perferendis repellendus reprehenderit eligendi quos
-            eum Eveniet fuga placeat ab eligendi!
+            quis inventore autem repudiandae Odio repellat quis
+            inventore autem repudiandae Odio repellat quis
+            inventore autem repudiandae Odio repellat placeat
+            necessitatibus dicta hic eveniet. Possimus iusto
+            placeat ipsa voluptatem ducimus Ad ullam pariatur
+            necessitatibus dicta hic eveniet. Possimus iusto
+            placeat ipsa voluptatem ducimus Ad ullam pariatur
+            ducimus amet omnis odio quidem Mollitia natus nihil
+            ullam veniam saepe quod Veniam nihil consequatur!
           </p>
           <Button href="#" className="btn btn--green">
             Book Now
