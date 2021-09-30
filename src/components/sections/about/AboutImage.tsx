9ca1f680c76eea2af3969c60ccc4780fdb8aa4bd
justin0979 <justinm0979@gmail.com>
2021-09-30 14:38:39 -0500

    refactor Header to use smaller jpg until certain resolution and/or width

--- src/components/sections/about/AboutImage.tsx
+++ src/components/sections/about/AboutImage.tsx
@@ -7,9 +7,9 @@ interface AboutImageProps {
 const AboutImage: React.FC<AboutImageProps> = (props) => {
   return (
     <img
-      src={props.src}
+      src={props.srclg}
       srcSet={`${props.src} 300w, ${props.srclg} 1000w`}
-      sizes="(max-width: 900px) 20vw, (max-width: 600px) 30vw, 300px"
+      sizes="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px"
       alt={`Photo ${props.id}`}
       className={`composition__photo composition__photo--p${props.id}`}
     />
